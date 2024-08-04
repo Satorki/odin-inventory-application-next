e34986605b3ce9eb30ef629a09b4bc48a9e08510
Satorki <sebastian.kedzier@gmail.com>
2024-08-04 13:24:52 +0200

    Route for index added

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 const app = express();
-
 app.set("view engine", "ejs");
 
 app.use("/", indexRoutes);
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.send("OK");
-});
-
-router.get("/index", (req, res) => {
   res.render("index", {
-    subject: "EJS template engine",
-    name: "our template",
-    link: "https://google.com",
+    title: "Odin Inventory App Next",
+    name: "Equipment",
   });
 });
 
