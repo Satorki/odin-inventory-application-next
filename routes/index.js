f2e53ac6807ab800d10825c2c2aa07d7a30eaca7
Satorki <sebastian.kedzier@gmail.com>
2024-08-05 10:51:27 +0200

    Add Item form added

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,23 @@ const asyncHandler = require("express-async-handler");
 router.get("/", (req, res) => {
   res.render("index", {
     title: "Odin Inventory App Next",
-    name: "Equipment",
+    description: "List of the cars:",
   });
 });
 
 // ADD CAR
+router.get("/add-item", (req,res) => {
+  res.render("addItem", {
+    title:"Add Item",
+    description: "Please Add a car"
+  })
+})
 router.post(
-  "/",
+  "/add-item",
   asyncHandler((req, res) => {
     console.log(req.body);
     res.status(200).send("Car added")
+    res.redirect("/")
   })
 );
 
