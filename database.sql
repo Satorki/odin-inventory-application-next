6a837d321465acc7b3e2493752579dd0d7e4d7c0
Satorki <sebastian.kedzier@gmail.com>
2024-08-08 19:19:34 +0200

    some database values added

--- database.sql
+++ database.sql
@@ -1,11 +1,29 @@
 CREATE DATABASE carshop;
 
-CREATE TABLE car_shop(
+CREATE TABLE car(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    company VARCHAR(255) NOT NULL,
-    category VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    company INTEGER NOT NULL,
+    category INTEGER NOT NULL,
     price DECIMAL(10, 2) NOT NULL
 );
 
-SELECT * FROM car_shop;
+CREATE TABLE category(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+);
+
+SELECT * FROM car;
+
+INSERT INTO company (name, description) VALUES ('Ford', 'Ford Motor Company (commonly known as Ford) is an American multinational automobile manufacturer headquartered in Dearborn, Michigan, United States.');
+INSERT INTO company (name, description) VALUES ('Lexus', 'Lexus (レクサス, Rekusasu) is the luxury vehicle division of the Japanese automaker Toyota Motor Corporation.');
+
+INSERT INTO car (name, description, company_id, category_id, price) VALUES ('Fiesta', 'The Ford Fiesta is a supermini car that was marketed by Ford from 1976 to 2023 over seven generations.', 1, 2, 1000);
+INSERT INTO car (name, description, company_id, category_id, price) VALUES ('IS', 'The Lexus IS (Japanese: レクサス・IS, Hepburn: Rekusasu IS) is a compact executive car[1] (D-segment in Europe) sold by Lexus, a luxury division of Toyota, since 1998.', 2, 1, 9000);
+INSERT INTO car (name, description, company_id, category_id, price) VALUES ('Mondeo', 'The Ford Mondeo is a car manufactured by Ford since 1993. The first Ford model declared as a "world car".', 1, 1, 3000);
+
+
+INSERT INTO category (name, description) VALUES ('Sedan', 'Most common kind of car on the road 4 doors');
+INSERT INTO category (name, description) VALUES ('Hatchback', 'Usually smaller than a sedan and has either 2 or 4 doors');
--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,45 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 
 router.get("/", (req, res) => {
+  const itemList = [
+    {
+      name: "car1",
+    },
+    { name: "car2" },
+    { name: "car3" },
+  ];
+
   res.render("index", {
     title: "Odin Inventory App Next",
     description: "List of the cars:",
+    itemList: itemList,
   });
 });
 
-// ADD CAR
-router.get("/add-item", (req,res) => {
+// ADD ITEM
+router.get("/add-item", (req, res) => {
   res.render("addItem", {
-    title:"Add Item",
-    description: "Please Add a car"
-  })
-})
+    title: "Add Item",
+    description: "Please Add a car",
+  });
+});
 router.post(
   "/add-item",
   asyncHandler((req, res) => {
     console.log(req.body);
-    res.status(200).send("Car added")
-    res.redirect("/")
+    res.status(200).send("Car added");
+    res.redirect("/");
   })
 );
+
+// SHOW ITEM
+// router.get("/:id", (req, res) => {
+//   const itemId = "id";
+
+//   res.render("item", {
+//     title: "item",
+//     itemData: itemId,
+//   });
+// });
 
 module.exports = router;
